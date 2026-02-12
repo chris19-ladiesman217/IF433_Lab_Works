cb697d03c5a3ec73e701e223b42f5cfdbbd1b524
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-02-12 10:52:25 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000115483_ChristopherRaymondPambudi.week01
+
+class GradingSystem
+fun main() {
+    var name: String = "Jhon Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai:" + score)
+}
