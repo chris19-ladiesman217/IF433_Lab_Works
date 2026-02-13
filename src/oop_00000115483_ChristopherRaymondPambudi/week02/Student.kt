891228b93bcae792d94075c6bda702cd4716f00d
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-02-13 15:13:05 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week02/Student.kt
@@ -0,0 +1,10 @@
+package oop_00000115483_ChristopherRaymondPambudi.week02
+
+class Student
+    (
+            val name: String,
+            val nim: String,
+            val major: String
+            ){
+
+}
