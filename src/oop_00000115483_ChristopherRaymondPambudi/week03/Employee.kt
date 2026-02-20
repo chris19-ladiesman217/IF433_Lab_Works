32049e123f81b561afe6900527344f881ebe9829
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-02-20 14:58:02 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000115483_ChristopherRaymondPambudi/week03/Employee.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000115483_ChristopherRaymondPambudi.week03
 
 class Employee(val name: String)  {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week03/Main.kt
@@ -0,0 +1,8 @@
+package oop_00000115483_ChristopherRaymondPambudi.week03
+
+class Main
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
