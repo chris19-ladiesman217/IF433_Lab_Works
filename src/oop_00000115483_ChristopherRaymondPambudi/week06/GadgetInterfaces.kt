d620327f7cbda156c5ed4fa34a2ba8fd42371f21
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-03-13 11:05:33 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week06/GadgetInterfaces.kt
@@ -0,0 +1,8 @@
+package oop_00000115483_ChristopherRaymondPambudi.week06
+
+class GadgetInterfaces interface camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
