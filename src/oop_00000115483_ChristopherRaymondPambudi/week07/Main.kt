9df5aa641a085c560be70e2bcd96122aa82e2e97
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-03-27 14:00:56 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000115483_ChristopherRaymondPambudi/week07/Main.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week07/Main.kt
@@ -5,6 +5,7 @@ fun main () {
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
     val uiMessage = when(response) {
+        is ApiResponse.Loading -> "Tampilkan Spinner"
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
     }
--- src/oop_00000115483_ChristopherRaymondPambudi/week07/SystemStates.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week07/SystemStates.kt
@@ -7,5 +7,5 @@ enum class Appstate {
 sealed class ApiResponse{
     data class Success(val data: String) : ApiResponse()
     data class Error(val message: String) : ApiResponse()
-    object loading : ApiResponse()
+    object Loading : ApiResponse()
 }
