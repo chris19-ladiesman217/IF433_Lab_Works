20c91960f1a7b445c84af0f6aa1dd7ece76915a8
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-03-27 13:36:09 +0700

    week07: implement data class for structural equality

--- src/oop_00000115483_ChristopherRaymondPambudi/week07/Main.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week07/Main.kt
@@ -1,6 +1,12 @@
 package oop_00000115483_ChristopherRaymondPambudi.week07
 
 fun main () {
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
+
     println("\n=== TEST REGULAR CLASS ===")
     val reg1 = RegularUser("Alice", 22)
     val reg2 = RegularUser("Alice", 22)
