38d5abcdfc28a1f45fbc48cb2a1a91147505e933
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-03-27 13:46:16 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000115483_ChristopherRaymondPambudi/week07/SystemStates.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_00000115483_ChristopherRaymondPambudi.week07
 enum class Appstate {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse{
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object loading : ApiResponse()
+}
