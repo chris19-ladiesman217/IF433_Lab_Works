f9daff220b2080ae7c0ca28d3334abe82fa53bc5
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-04-17 15:29:08 +0700

    week08: implement safe call with let block

--- src/oop_00000115483_ChristopherRaymondPambudi/week08/Main.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week08/Main.kt
@@ -7,4 +7,14 @@ fun main() {
     // Rantai Safe Calss yang elegan
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
     println("Tujuan Pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+    val receipt = validOrder.totalPrice?.let { price ->
+        // Blok ini hanya jalan jika totalPrice tidak null.
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+    println(receipt)
+
 }
