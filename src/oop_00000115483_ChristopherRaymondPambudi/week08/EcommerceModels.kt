1959bfa96430c8e4f04e9badb744a08e05640b71
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-04-17 18:02:55 +0700

    week08:(task)createsealedclassProductanddataclasses

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week08/EcommerceModels.kt
@@ -0,0 +1,5 @@
+package oop_00000115483_ChristopherRaymondPambudi.week08
+
+sealed class Product
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+data class Clothing(val id: String, val name: String, val size: String) : Product()
