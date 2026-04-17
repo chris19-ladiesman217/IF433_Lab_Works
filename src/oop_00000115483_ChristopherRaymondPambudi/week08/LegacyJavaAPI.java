10196350220384e5749c51fdf2190dd3dd57f877
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-04-17 16:07:19 +0700

    week08:createLegacyJavaAPIclassinJava

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000115483_ChristopherRaymondPambudi.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi Ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
