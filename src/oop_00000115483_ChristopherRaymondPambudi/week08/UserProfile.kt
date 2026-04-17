3f96aafa7addbc061e2ee4d8342fed143c8bed21
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-04-17 15:40:15 +0700

    week08: create mixed data collection

--- src/oop_00000115483_ChristopherRaymondPambudi/week08/UserProfile.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week08/UserProfile.kt
@@ -3,6 +3,6 @@ package oop_00000115483_ChristopherRaymondPambudi.week08
 // name adalah Non-Null (wajib), email dan phone adalah Nullable (opsional)
 class UserProfile(
     val name: String,
-    val email: String,
+    val email: String?,
     val phone: String? = null // Default argument
 )
