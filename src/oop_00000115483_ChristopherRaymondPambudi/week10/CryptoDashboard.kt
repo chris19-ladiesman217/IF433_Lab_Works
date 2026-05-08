ceebe47dbb0abba30cbc2d33c65a76ac4172e443
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-08 10:49:50 +0700

    week10: (task) populate coin repository

--- src/oop_00000115483_ChristopherRaymondPambudi/week10/CryptoDashboard.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000115483_ChristopherRaymondPambudi.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.523))
+    coinRepo.add(Coin("ETH", 4.12))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
