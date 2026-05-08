6c28c90bb2759857a37296fe93c3b103ace42c4c
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-08 10:53:56 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000115483_ChristopherRaymondPambudi/week10/CryptoDashboard.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week10/CryptoDashboard.kt
@@ -8,4 +8,10 @@ fun main() {
     coinRepo.add(Coin("USDT", 1500.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
+
+    println("=== CRYPTO WALLET DASHBOARD ===")
+    println("Status: ${response.status}")
+    response.data.forEach {
+        println("Asset: ${it.name} | Balance: ${it.balance}")
+    }
 }
