a92d0a5a7210c11ff126f5eadfbab76e0fa60830
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-15 12:48:11 +0700

    week12: (task) implement dispenseKibble function

--- src/oop_00000115483_ChristopherRaymondPambudi/week12/SmartFeeder.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week12/SmartFeeder.kt
@@ -1,3 +1,14 @@
 package oop_00000115483_ChristopherRaymondPambudi.week12
 
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
 
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    return availableGram
+}
