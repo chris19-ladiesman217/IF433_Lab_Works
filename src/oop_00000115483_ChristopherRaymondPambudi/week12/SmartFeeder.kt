a0052265b5da1d705b54bd9144696497bbb88759
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-15 13:03:56 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000115483_ChristopherRaymondPambudi/week12/SmartFeeder.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week12/SmartFeeder.kt
@@ -28,4 +28,18 @@ fun dispenseKibble(
 fun main() {
     var currentKibbleStock = 50
     println("Stok awal kibble: $currentKibbleStock gr")
+
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        currentKibbleStock = dispenseKibble(80, currentKibbleStock, false)
+    }
+    catch (e: DispenserJamException) {
+        println("Caught: ${e.message}")
+    }
+    catch (e: FoodEmptyException) {
+        println("Caught: ${e.message}")
+    }
+    catch (e: Exception) {
+        println("Caught General Error: ${e.message}")
+    }
 }
