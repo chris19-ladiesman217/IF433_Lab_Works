f03ff59ebad001cc40c8438dcb71892bf7ca732e
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-15 11:51:37 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000115483_ChristopherRaymondPambudi.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
