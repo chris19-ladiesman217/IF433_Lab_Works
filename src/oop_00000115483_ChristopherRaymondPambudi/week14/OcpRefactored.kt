f0f1a4eed03d1339ec06743d32b421a25e212d8e
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-29 11:35:08 +0700

    week14: refactor DiscountCalculator to use strategy

--- src/oop_00000115483_ChristopherRaymondPambudi/week14/OcpRefactored.kt
+++ src/oop_00000115483_ChristopherRaymondPambudi/week14/OcpRefactored.kt
@@ -12,3 +12,7 @@ class StudentDiscount : DiscountStrategy {
 class MemberDiscount : DiscountStrategy {
     override fun apply(price: Double) = price * 0.85
 }
+
+class SafeDiscountCalculator(private val strategy: DiscountStrategy) {
+    fun calculate(price: Double) = strategy.apply(price)
+}
