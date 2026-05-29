99502a110837a0dea6e61d59e175556e232eb3f3
Christopher Raymond Pambudi <cr.pambudi@gmail.com>
2026-05-29 11:32:09 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000115483_ChristopherRaymondPambudi/week14/OcpRefactored.kt
@@ -0,0 +1,14 @@
+package oop_00000115483_ChristopherRaymondPambudi.week14
+
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
